28052025610f00fa77d9a6037d31b25a0ab97746
temp20230620 <137216868+temp20230620@users.noreply.github.com>
2023-06-20 14:34:54 -0400

    Create .pre-commit-hooks.yaml

--- yaml_shellcheck.py
+++ yaml_shellcheck.py
@@ -415,7 +415,7 @@ def cleanup_files(args):
         logger.debug("removed working dir %s", args.outdir)
 
 
-if __name__ == "__main__":
+def main():
     args = setup()
 
     filenames = []
@@ -431,3 +431,7 @@ if __name__ == "__main__":
     cleanup_files(args)
     # exit with shellcheck exit code
     sys.exit(check_proc_result.returncode if check_proc_result else 0)
+
+
+if __name__ == "__main__":
+    main()
